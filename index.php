2fac9a1d2ecffcec8b07190798f309bc73bfbd09
franciscoblancojn <blancofrancisco34@gmail.com>
2025-03-05 15:28:37 -0500

    fix

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ define("AVSHME_BASENAME",plugin_basename(__FILE__));
 define("AVSHME_DIR",plugin_dir_path( __FILE__ ));
 define("AVSHME_URL",plugin_dir_url(__FILE__));
 
-require_once AVWG_DIR . 'update.php';
+require_once AVSHME_DIR . 'update.php';
 github_updater_plugin_wordpress([
     'basename'=>AVSHME_BASENAME,
     'dir'=>AVSHME_DIR,
