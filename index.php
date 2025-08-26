97b3ad15d7cc8d49456a6c673dadad8f63c9056e
franciscoblancojn <blancofrancisco34@gmail.com>
2025-08-26 12:19:44 -0500

    3.3.0

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 Plugin Name: Aveonline Shipping
 Plugin URI: https://github.com/franciscoblancojn/aveonline-shipping
 Description: Integración de woocommerce con los servicios de envío de Aveonline.
-Version: 3.2.1
+Version: 3.3.0
 Author: franciscoblancojn
 Author URI: https://franciscoblanco.vercel.app/
 License: GPL2+
