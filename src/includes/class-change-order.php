b97c0774c625e0ef38adf9d3c96beceb70e73cce
Francisco Blanco <blancofrancisco34@gmail.com>
2026-05-12 11:21:09 -0500

    validacion de checkout

--- src/includes/class-change-order.php
+++ src/includes/class-change-order.php
@@ -1,69 +1,73 @@
-<?php 
+<?php
+
 /**
-* AVSHME_add_function_order_change funcion para detectar el cambio de estado de pedidos para ejecutar peticiones al api de aveonline
-*
-* @access public
-* @return void
-*/
-function AVSHME_add_function_order_change($order_id) {
+ * AVSHME_add_function_order_change funcion para detectar el cambio de estado de pedidos para ejecutar peticiones al api de aveonline
+ *
+ * @access public
+ * @return void
+ */
+function AVSHME_add_function_order_change($order_id)
+{
     AVSHME_generate_guia($order_id);
 }
-add_action('woocommerce_order_status_processing',   'AVSHME_add_function_order_change' , 10, 1);  
-function AVSHME_generate_guia($order_id){
+add_action('woocommerce_order_status_processing',   'AVSHME_add_function_order_change', 10, 1);
+function AVSHME_generate_guia($order_id)
+{
     global $wpdb, $woocommerce, $current_user;
-    $order = wc_get_order( $order_id );
+    $order = wc_get_order($order_id);
     $settings = AVSHME_get_settings_aveonline();
     $api = new AveonlineAPI($settings);
 
     $order_data = $order->get_data();
     $e = array();
-    foreach ($order->get_items( 'shipping' ) as $item) {
+    foreach ($order->get_items('shipping') as $item) {
         foreach ($item->get_meta_data() as $data) {
-            $e[$data->get_data()["key"]] = json_decode(base64_decode($data->get_data()["value"]),true);
+            $e[$data->get_data()["key"]] = json_decode(base64_decode($data->get_data()["value"]), true);
         }
     }
     AVSHME_addLogAveonline(array(
-        "type"=>"AVSHME_generate_guia_request",
-        "destino"=>$e["request"],
+        "type" => "AVSHME_generate_guia_request",
+        "destino" => $e["request"],
     ));
-    if(!isset($e["request"])){
+    if (!isset($e["request"])) {
         return;
     }
     add_post_meta($order_id, 'AVSHME_generate_guia_request', json_encode($e["request"]));
     $r = $api->AVSHME_generate_guia($e["request"], $order);
     AVSHME_addLogAveonline(array(
-        "type"=>"AVSHME_generate_guia_r",
-        "destino"=>$r,
+        "type" => "AVSHME_generate_guia_r",
+        "destino" => $r,
     ));
     add_post_meta($order_id, 'AVSHME_generate_guia', json_encode($r));
-    if($r->status == "ok"){
+    if ($r->status == "ok") {
         $guia = $r->resultado->guia;
         unset($guia->archivoguia);
         unset($guia->archivorotulo);
         AVSHME_addLogAveonline(array(
-            "type"=>"AVSHME_generate_guia_guia",
-            "destino"=>$guia,
+            "type" => "AVSHME_generate_guia_guia",
+            "destino" => $guia,
         ));
 
-        AVSHME_update_options( $order_id, 'enable_recogida', true);
-        AVSHME_update_options( $order_id, 'guias_rotulos', $guia);
-        
+        AVSHME_update_options($order_id, 'enable_recogida', true);
+        AVSHME_update_options($order_id, 'guias_rotulos', $guia);
+
         $respond = $api->system_update_guia(array(
             'numguia'   => $guia->numguia,
             'order_id'  => $order_id,
-            'transportadora_id'=>$e["request"]['idtransportador']
+            'transportadora_id' => $e["request"]['idtransportador']
         ));
-    }else{
-        AVSHME_update_options( $order_id, 'error_guia', json_encode($r));
+    } else {
+        AVSHME_update_options($order_id, 'error_guia', json_encode($r));
     }
 }
 //show
-add_action( 'woocommerce_admin_order_data_after_billing_address', 'AVSHME_order_pdf', 10, 1 );
-function AVSHME_order_pdf( $order ) {    
-	$order_id = $order->get_id();
-	$guias_rotulos = AVSHME_get_options( $order_id, 'guias_rotulos' );
-	$enable_recogida = AVSHME_get_options( $order_id, 'enable_recogida' );
-    $error_guia = AVSHME_get_options( $order_id, 'error_guia');
+add_action('woocommerce_admin_order_data_after_billing_address', 'AVSHME_order_pdf', 10, 1);
+function AVSHME_order_pdf($order)
+{
+    $order_id = $order->get_id();
+    $guias_rotulos = AVSHME_get_options($order_id, 'guias_rotulos');
+    $enable_recogida = AVSHME_get_options($order_id, 'enable_recogida');
+    $error_guia = AVSHME_get_options($order_id, 'error_guia');
 
     // var_dump(
     //     array([
@@ -73,36 +77,121 @@ function AVSHME_order_pdf( $order ) {
     //     ])
     // );
 
-    if($error_guia){
-        $error_guia = json_decode($error_guia,true);
-        ?>
+    if ($error_guia) {
+        $error_guia = json_decode($error_guia, true);
+?>
         <div style="color:red;">
             <strong>Ocurrio un Error:</strong>
             <br>
-            <?=$error_guia["message"]?>
+            <?= $error_guia["message"] ?>
         </div>
-        <?php
+    <?php
 
     }
-	if ( $guias_rotulos ) {
-        ?>
+    if ($guias_rotulos) {
+    ?>
         <strong>Guia:</strong>
         <br>
-        <a target="_blank" href="<?=$guias_rotulos->rutaguia;?>">
-            <?=$guias_rotulos->mensaje;?>
+        <a target="_blank" href="<?= $guias_rotulos->rutaguia; ?>">
+            <?= $guias_rotulos->mensaje; ?>
         </a>
         <br>
         <strong>Rotulo:</strong>
         <br>
-        <a target="_blank" href="<?=$guias_rotulos->rotulo;?>">
-            <?=$guias_rotulos->numguia;?>
+        <a target="_blank" href="<?= $guias_rotulos->rotulo; ?>">
+            <?= $guias_rotulos->numguia; ?>
         </a>
-        <?php
+    <?php
     }
-    if($enable_recogida){
-        ?>
+    if ($enable_recogida) {
+    ?>
         <br>
         <strong>Listo para generar Recogida</strong>
-        <?php
+<?php
     }
 }
+
+add_action('woocommerce_checkout_process', function () {
+    $LOG = [
+        "type" => "woocommerce_checkout_process",
+    ];
+    try {
+
+        // Método de pago seleccionado
+        $payment_method = isset($_POST['payment_method'])
+            ? wc_clean($_POST['payment_method'])
+            : '';
+        $LOG['payment_method'] = $payment_method;
+        // Métodos de envío seleccionados
+        $shipping_methods = isset($_POST['shipping_method'])
+            ? (array) $_POST['shipping_method']
+            : [];
+
+        if (empty($shipping_methods)) {
+            return;
+        }
+
+        /**
+         * Detectar si el envío seleccionado
+         * es contraentrega
+         */
+        $is_shipping_contraentrega = false;
+
+        foreach ($shipping_methods as $method) {
+
+            /**
+             * Ejemplo IDs:
+             *
+             * transportadora_wc_contraentrega_on
+             * transportadora_wc_contraentrega_off
+             */
+
+            if (strpos($method, 'wc_contraentrega_on') !== false) {
+                $is_shipping_contraentrega = true;
+                break;
+            }
+        }
+
+        /**
+         * Detectar si el pago es contraentrega
+         */
+        $is_payment_contraentrega =
+            $payment_method === AVSHME_PAYMENT_CONTRAENTREGA;
+
+        /**
+         * VALIDACIONES
+         */
+
+        // Pago contraentrega PERO envío normal
+        if (
+            $is_payment_contraentrega &&
+            !$is_shipping_contraentrega
+        ) {
+
+            wc_add_notice(
+                __('Debes seleccionar un envío contraentrega para usar pago contraentrega.', 'woocommerce'),
+                'error'
+            );
+
+            return;
+        }
+
+        // Pago normal PERO envío contraentrega
+        if (
+            !$is_payment_contraentrega &&
+            $is_shipping_contraentrega
+        ) {
+
+            wc_add_notice(
+                __('No puedes seleccionar un envío contraentrega con un método de pago normal.', 'woocommerce'),
+                'error'
+            );
+
+            return;
+        }
+    } catch (\Throwable $th) {
+        //throw $th;
+    } finally {
+        AVSHME_addLogAveonline($LOG);
+    }
+});
