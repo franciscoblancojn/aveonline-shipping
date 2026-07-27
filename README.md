5d6c73da506c28086fc0b84656afe5a132085585
Francisco Blanco <blancofrancisco34@gmail.com>
2026-07-27 12:10:05 -0500

    Release 4.1.2

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 
 **Tested up to:** 6.7
 
-**Stable tag:** 4.1.1
+**Stable tag:** 4.1.2
 
 **License:** GPLv2 or later
 
--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 Plugin Name: Aveonline Shipping
 Plugin URI: https://github.com/franciscoblancojn/aveonline-shipping
 Description: Integración de woocommerce con los servicios de envío de Aveonline.
-Version: 4.1.1
+Version: 4.1.2
 Author: franciscoblancojn
 Author URI: https://franciscoblanco.vercel.app/
 License: GPL2+
